6bc09bad68f567caeadbca67d135020bd116d00e
nao mac <sagalumaata@gmail.com>
2023-05-21 18:27:09 +0900

    一旦タイム管理以外完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import {calculateWinner} from "../src/library/calculateWinner"
 
 type SquareType = {
 	value:number|string
@@ -35,8 +36,6 @@ type SquareType = {
 // 	return <button className="square">{value}</button>
 // }
 
-
-
 // function Square({value}:{value:number}) {
 // 	return <button className="square">{value}</button>;
 // }
@@ -45,38 +44,95 @@ type SquareType = {
 // 	return <button className="square">{value}</button>
 // }
 
-function Square({value}:{value:string}){
-	// const [value,setValue] = useState()
-	// function handleClick() {
-	// 	setValue('X');
-	// }
-	return <button className="square">{value}</button>
+type SquareProps ={
+	value:number;
+	onSquareClick?:()=> void
+
+}
+function Square(props:SquareProps){
+	return <button className="square" onClick={props.onSquareClick}>{props.value}</button>
 }
 
 
-export default function Board():JSX.Element {
-	const [squares,setSquares] = useState(Array(9).fill(null))
+
+
+
+function Board():JSX.Element {
+	const [squares,setSquares] = useState(Array(9).fill(''))
+	const [xIsNext,setNext] = useState(true);
+	const [gameStatus,setgameStatus] = useState('ゲームを始めます');	
+
+
+	function handleClick(i:number){
+		//同じ箇所をクリックししてる、またはすでに終了していたら終了
+		if(squares[i] || calculateWinner(squares)) return
+
+		
+		const nextSquares = squares.slice();
+		
+		//次の番手をセット
+		nextSquares[i] = xIsNext ? 'X' : '○';
+
+		//盤石に新しい情報をセット
+		setSquares(nextSquares)
+
+		//勝者判定
+		const winner = calculateWinner(nextSquares);
+		
+		//次のプレイヤー（trueかかfalseか）を定義
+		setNext(!xIsNext)	
+		
+		if(winner){
+			setgameStatus("Winner: " + winner);
+		}else{
+			setgameStatus("Next player: " + (xIsNext ? 'X' : '○'));
+		}
+
+	}
+
 	return (
 		<>
+		    <div className="status">{gameStatus}</div>
 			<div className="board-row">
-				{/* 文字列の場合は{}で包まなくてもいい} */}
-				<Square value={squares[0]}/>
-				<Square value={squares[1]}/>
-				<Square value={squares[2]}/>
-			</div>
-			<div className="board-row">
-				<Square value={squares[3]}/>
-				<Square value={squares[4]}/>
-				<Square value={squares[5]}/>
+				{/* propsは文字列の場合は{}で包まなくてもいい} */}
+
+
+				{/* これだと返り値が入ってしまうので、エラーになる。 */}
+				{/* <Square value={squares[0]} onClick={handleClick(0)}/> */}
+
+				<Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+				<Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+				<Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+
 
 			</div>
 			<div className="board-row">
-				<Square value={squares[6]}/>
-				<Square value={squares[7]}/>
-				<Square value={squares[8]}/>
+				<Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+				<Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+				<Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+
+			</div>
+			<div className="board-row">
+				<Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+				<Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+				<Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
 
 			</div>
       
 		</>
-		)
+	)
+}
+
+
+//これをデフォルトのコンポーネントとしてエクスポートする。
+// board、gameコンポーネントにはexport defaultの記述がないのはそういうこと。
+export default function Game(){
+
+	return (
+		<div className="game">
+			<div className="game-board">
+				<Board/>
+			</div>
+		</div>
+	)
 }
